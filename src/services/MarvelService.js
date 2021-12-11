5229701dd8d99deae2bd0eafcf6c44c8e936fcc3
VinsFlo <vinsflorineus@gmail.com>
2021-12-11 11:04:42 +0300

    fix

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
 	_apiBase = 'https://gateway.marvel.com:443/v1/public/';
-	_apiKey = '744534304ffacc290564d8cbde277a91';
+	_apiKey = 'apikey=744534304ffacc290564d8cbde277a91';
 
 	getResource = async (url) => {
 		let res = await fetch(url);
