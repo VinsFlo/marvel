45549d8aa10d0f9f145f17643976137891344cab
VinsFlo <vinsflorineus@gmail.com>
2021-12-31 10:24:30 +0300

    comic page

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,13 +3,15 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ComicsList from "../comicsList/ComicsList";
+import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
 const App = () => {
 
-	const [selectedChar, setChar] = useState(null); //объявление новой переменной состояния selectedChar
+	const [selectedChar, setChar] = useState(null);
 
 	const onCharSelected = (id) => {
 		setChar(id);
@@ -19,18 +21,20 @@ const App = () => {
 		<div className="app">
 			<AppHeader />
 			<main>
-				<ErrorBoundary>
-					<RandomChar />
-				</ErrorBoundary>
-				<div className="char__content">
-					<ErrorBoundary>
-						<CharList onCharSelected={onCharSelected} />
-					</ErrorBoundary>
-					<ErrorBoundary>
-						<CharInfo charId={selectedChar} />
-					</ErrorBoundary>
-				</div>
-				<img className="bg-decoration" src={decoration} alt="vision" />
+				{/* { <ErrorBoundary>
+                    <RandomChar/>
+                </ErrorBoundary>
+                <div className="char__content">
+                    <ErrorBoundary>
+                        <CharList onCharSelected={onCharSelected}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                </div>
+                <img className="bg-decoration" src={decoration} alt="vision"/> }
+				<AppBanner /> */}
+				<ComicsList />
 			</main>
 		</div>
 	)
