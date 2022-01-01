f6e66c0070647d12cb986b39a20e3dcbadefd2c2
VinsFlo <vinsflorineus@gmail.com>
2022-01-01 18:23:52 +0300

    fix

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,8 +3,6 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import AppBanner from "../appBanner/AppBanner";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
@@ -21,23 +19,55 @@ const App = () => {
 		<div className="app">
 			<AppHeader />
 			<main>
-				{/* { <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> }
-				<AppBanner /> */}
-				<ComicsList />
+				<ErrorBoundary>
+					<RandomChar />
+				</ErrorBoundary>
+				<div className="char__content">
+					<ErrorBoundary>
+						<CharList onCharSelected={onCharSelected} />
+					</ErrorBoundary>
+					<ErrorBoundary>
+						<CharInfo charId={selectedChar} />
+					</ErrorBoundary>
+				</div>
+				<img className="bg-decoration" src={decoration} alt="vision" />
 			</main>
 		</div>
 	)
 }
 
 export default App;
+
+
+
+// import { useState } from "react";
+// import AppHeader from "../appHeader/AppHeader";
+// import RandomChar from "../randomChar/RandomChar";
+// import CharList from "../charList/CharList";
+// import CharInfo from "../charInfo/CharInfo";
+// import ComicsList from "../comicsList/ComicsList";
+// import AppBanner from "../appBanner/AppBanner";
+// import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+
+// import decoration from '../../resources/img/vision.png';
+
+// const App = () => {
+
+// 	const [selectedChar, setChar] = useState(null);
+
+// 	const onCharSelected = (id) => {
+// 		setChar(id);
+// 	}
+
+// 	return (
+// 		<div className="app">
+// 			<AppHeader />
+// 			<main>
+// 				{ }
+// 				<ComicsList />
+// 			</main>
+// 		</div>
+// 	)
+// }
+
+// export default App;
